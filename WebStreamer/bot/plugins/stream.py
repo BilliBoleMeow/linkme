531f3098e5b6b0450b7fc9c82fc7edb7801cc335
BilliBoleMeow <88665437+BilliBoleMeow@users.noreply.github.com>
2022-06-27 11:34:06 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -39,7 +39,7 @@ async def media_receive_handler(_, m: Message):
         # dkabl
         rm = None
     await m.reply_text(
-        text="<code>Copy The Download Link From Below Or Tap On The Download Now Button</code>\n(<a href='{}'>shortened</a>)".format(
+        text="<code>Copy The Download Link From Below Or Tap On The Download Now Button</code>\n       (<a href='{}'>Download Link</a>)".format(
                      short_link
         ),
         quote=True,
