1af9d4e3ee9c39ef15ea39f92e3076c380461e0f
BilliBoleMeow <88665437+BilliBoleMeow@users.noreply.github.com>
2022-06-27 11:31:24 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -9,5 +9,5 @@ from WebStreamer.bot import StreamBot
 @StreamBot.on_message(filters.command(["start", "help"]))
 async def start(_, m: Message):
     await m.reply(
-        f'Hi {m.from_user.mention(style="md")},\n Send me a file to get an instant stream/download link.\n <b>This Bot is Part Of @LiquidXProjects. </b>'
+        f'Hi {m.from_user.mention(style="md")},\nSend me a file to get an instant stream/download link.\n<b>This Bot is Part Of @LiquidXProjects. </b>'
     )
