dc45d1a6184cc1e929d6b1ea11680e4be76fc264
BilliBoleMeow <88665437+BilliBoleMeow@users.noreply.github.com>
2022-06-27 12:18:31 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -14,4 +14,4 @@ async def start(_, m: Message):
 A Simple Telegram File To Link Generation Bot.""",
             parse_mode=ParseMode.HTML,
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup( [ [InlineKeyboardButton('LiquidX Projects', url=f"https://t.me/liquidxprojects"))
+            reply_markup=InlineKeyboardMarkup( [ [InlineKeyboardButton('LiquidX Projects', url=f"https://t.me/liquidxprojects")))
